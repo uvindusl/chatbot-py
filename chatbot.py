3c3e8d43a87d43963e28ec20c4d232396a3ba198
Uvindu Sankalpa <uvindusankalpa2021@gmail.com>
2025-04-01 00:45:51 +0530

    Add functionality to generate AI content using Google GenAI client

--- chatbot.py
+++ chatbot.py
@@ -1,1 +1,10 @@
 from google import genai
+
+from google import genai
+
+client = genai.Client(api_key="AIzaSyA5eqw2zMyAjqtI8-o5KM04aSg7y-n5Z_U")
+
+response = client.models.generate_content(
+    model="gemini-2.0-flash", contents="Explain how AI works in a few words"
+)
+print(response.text)
